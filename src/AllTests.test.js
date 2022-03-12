555e6ef99fc2d6f51a2e148d519f29b25628edfe
Maciej Moczadło <maciek.moczadlo@gmail.com>
2022-03-12 20:22:01 +0100

    Working tests to application

---
+++ src/AllTests.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  render,
+  screen,
+  waitFor,
+  queryByAttribute,
+} from "@testing-library/react";
+import LocalTime from "./Forecasts/LocalTime";
+import SunTime from "./Forecasts/SunTime";
+import TodayForecast from "./Forecasts/TodayForecast";
+import FiveDayForecast from "./Forecasts/FiveDayForecast";
+import App from "./App";
+
+describe("LocalTime test", () => {
+  it("should render local time for Poznan", async () => {
+    render(<LocalTime cityName="Poznan" />);
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    await waitFor(() => {
+      screen.queryByText(time);
+    });
+  });
+});
+
+describe("SunTime test", () => {
+  it("should render local suntime for Poznan", async () => {
+    const getById = queryByAttribute.bind(null, "id");
+    const dom = render(<SunTime cityName="Poznan" />);
+    await waitFor(() => {
+      screen.queryByText("AM");
+      screen.queryByText("PM");
+    });
+  });
+});
+
+describe("TodayForecast test", () => {
+  it("should render today forecast for Poznan", async () => {
+    const getById = queryByAttribute.bind(null, "id");
+    const dom = render(<TodayForecast cityName="Poznan" />);
+    await waitFor(() => {
+      screen.queryByText(" °C");
+      screen.queryByText("m/s S");
+    });
+  });
+});
+describe("FiveDayForecast test", () => {
+  it("should render today forecast for Poznan", async () => {
+    const getById = queryByAttribute.bind(null, "id");
+    const dom = render(<FiveDayForecast cityName="Poznan" />);
+    await waitFor(() => {
+      screen.queryByText(" °C");
+    });
+  });
+});
--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,10 @@ import App from "./App";
 test("renders component", async () => {
   const { getByText } = render(<App />);
   expect(getByText("Pogoda dla")).toBeInTheDocument();
+  expect(
+    getByText("Wybierz miasto dla którego wyświetlić pogodę.")
+  ).toBeInTheDocument();
+  expect(
+    getByText("Jeśli nie wyświetlają się dane pogody to trzeba zmienić klucz.")
+  ).toBeInTheDocument();
 });
